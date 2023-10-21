6d0594c6a5db7da70bb6a51aafb5927031ee587c
Volodymyr Kravchuk <volodymyr.kravchuk@socialtechnologies.io>
2023-10-21 20:11:23 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/SysProgLab3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SysProgLab3.dir/main.cpp.o"
+  "CMakeFiles/SysProgLab3.dir/main.cpp.o.d"
+  "SysProgLab3"
+  "SysProgLab3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SysProgLab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
